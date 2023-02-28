a7cb909de765390d6a613f7d0c1c666f5b24f80c
Jason Graves <godlikemouse@godlikemouse.com>
2023-02-27 23:25:06 -0700

    Updated tests for HelloWorld

--- test/HelloWorld.js
+++ test/HelloWorld.js
@@ -3,6 +3,12 @@ const HelloWorld = artifacts.require("HelloWorld");
 contract("HelloWorld", () => {
     it("Smart contract deployed", async () => {
         const helloWorld = await HelloWorld.deployed();
-        assert(HelloWorld.address != "");
+        assert(helloWorld.address != "");
+    });
+
+    it("hello method", async () => {
+	    const helloWorld = await HelloWorld.deployed();
+	    const response = await helloWorld.hello();
+	    assert(response == "Hello World");
     });
 });
